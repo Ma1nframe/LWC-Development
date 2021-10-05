b417763cb42940010a54a456b387db5020548b16
AJ <85966858+Ma1nframe@users.noreply.github.com>
2021-10-05 12:20:48 -0400

    mounting

--- force-app/main/default/lwc/lifecycleChild/lifecycleChild.js
+++ force-app/main/default/lwc/lifecycleChild/lifecycleChild.js
@@ -1,3 +1,30 @@
 import { LightningElement } from 'lwc';
 
-export default class LifecycleChild extends LightningElement {}
+export default class LifecycleChild extends LightningElement {
+
+    constructor() {
+        super()
+        // Can't use querySelector yet because html is not yet rendered. We can only use this.template to add something likde a listener.
+        // this.template.addEventListener('mouseover' function(){})
+        console.log('Child constructor() called');
+    }
+
+    connectedCallback() {
+        // Used to fetch data on page from seerver while component loads
+        // Calls to Apex can be made here
+        console.log('Child connectedCallback() called');
+    }
+
+    renderedCallback() {
+        console.log('Child renderedCallback() called');
+    }
+
+
+/***** THIS WILL NOT WORK AS IT DOES IN PARENT *****/
+//     // Creating property name
+//     foo;
+//     // Creating changeHandler
+//     changeHandler(event) {
+//         this.name = event.target.value
+//     }
+}
--- force-app/main/default/lwc/lifecycleParent/lifecycleParent.js
+++ force-app/main/default/lwc/lifecycleParent/lifecycleParent.js
@@ -1,3 +1,30 @@
 import { LightningElement } from 'lwc';
 
-export default class LifecycleParent extends LightningElement {}
+export default class LifecycleParent extends LightningElement {
+
+    constructor() {
+        super()
+        // Can't use querySelector yet because html is not yet rendered. We can only use this.template to add something likde a listener.
+        // this.template.addEventListener('mouseover' function(){})
+        console.log('Parent constructor() called');
+    }
+
+    connectedCallback() {
+        // Used to fetch data on page from seerver while component loads
+        // Calls to Apex can be made here
+        console.log('Parent connectedCallback() called');
+    }
+
+    // This callback is called LOADS of times. In fact it can be called eveytime you write a character from your keyboard.
+    renderedCallback() {
+        console.log('Parent renderedCallback() called');
+    }
+
+    // Creating property name
+    name;
+    // Creating changeHandler
+    changeHandler(event) {
+        this.name = event.target.value
+    }
+
+}
