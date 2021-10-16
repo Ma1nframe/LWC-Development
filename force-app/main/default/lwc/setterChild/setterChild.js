b58e839a93ec32d9abb030bfcb90bbddd578a71f
AJ <85966858+Ma1nframe@users.noreply.github.com>
2021-10-16 16:27:20 -0400

    "

---
+++ force-app/main/default/lwc/setterChild/setterChild.js
@@ -0,0 +1,26 @@
+import { LightningElement, api } from 'lwc';
+
+export default class SetterChild extends LightningElement {
+
+    userDetails
+
+    @api 
+    get detail() {
+        // It's important to use get because the data is dynamically changed by set
+        return this.userDetails
+    }
+
+    set detail(data) {
+        // Mutate data by creating a shallow copy w/ spread operator 
+        let newAge = data.userAge*2
+        // Spread operator rule: Right side data overides data of left side. Also it can add new properties.
+        this.userDetails= {...data, userAge:newAge, "location":"Boston"} 
+
+        /*** Primitive */
+        /*
+        // whatever data comes in updates the userDetails
+        this.userDetails = data
+        */
+    }
+
+}
---
+++ force-app/main/default/lwc/setterParent/setterParent.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class SetterParent extends LightningElement {
+
+    userDetails = {
+        userName: "Anthony",
+        userAge: 35
+    }
+
+    handleClick() {
+        
+    }
+
+}
