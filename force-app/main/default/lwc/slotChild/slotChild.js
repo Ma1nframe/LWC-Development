d8b68419a62f6bab91c1759aa65098eb726abe92
AJ <85966858+Ma1nframe@users.noreply.github.com>
2021-10-16 20:19:16 -0400

    slot communication

--- force-app/main/default/lwc/slotChild/slotChild.js
+++ force-app/main/default/lwc/slotChild/slotChild.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
 
-export default class SlotChild extends LightningElement {}
+export default class SlotChild extends LightningElement {
+
+    handleFooterChange() {
+        const footerElem = this.template.querySelector('.slds-card__footer')
+        if(footerElem) {
+            footerElem.classList.remove('slds-hide')
+        }
+    }
+    handleDivChange() {
+        const hiddenElemMsg = this.template.querySelector('.dhe')
+        if(hiddenElemMsg) {
+            hiddenElemMsg.classList.remove('slds-hide')
+        }
+    }
+
+}
